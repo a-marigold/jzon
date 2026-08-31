21c848f3cc3b0921e7619f98b9a59d57ec8e2e7c
marigold <phelixgluy@gmail.com>
2026-08-31 17:36:58 +0500

    build.zig: add check step

--- build.zig
+++ build.zig
@@ -3,49 +3,26 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const mod = b.addModule("jzon", .{
+
+    const module = b.addModule("jzon", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
+        .optimize = optimize,
     });
-    const exe = b.addExecutable(.{
+
+    const checkStep = b.step("check", "Semantic analysis");
+    const checkExe = b.addExecutable(.{
         .name = "jzon",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-            .imports = &.{
-                .{ .name = "jzon", .module = mod },
-            },
-        }),
+        .root_module = module,
     });
-
-    b.installArtifact(exe);
-
-    const run_step = b.step("run", "Run the app");
-
-    const run_cmd = b.addRunArtifact(exe);
-
-    run_step.dependOn(&run_cmd.step);
-
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    run_cmd.addPassthruArgs();
+    checkExe.step.dependOn(checkStep);
 
     const mod_tests = b.addTest(.{
-        .root_module = mod,
+        .root_module = module,
     });
 
     const run_mod_tests = b.addRunArtifact(mod_tests);
-
-    const exe_tests = b.addTest(.{
-        .root_module = exe.root_module,
-    });
-
-    const run_exe_tests = b.addRunArtifact(exe_tests);
-
     const test_step = b.step("test", "Run tests");
 
     test_step.dependOn(&run_mod_tests.step);
-
-    test_step.dependOn(&run_exe_tests.step);
 }
