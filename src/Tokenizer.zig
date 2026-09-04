cc7a86501944f55c15a7ed2225211db16f0aa064
marigold <phelixgluy@gmail.com>
2026-09-04 12:04:26 +0500

    Tokenizer: in all terms, rename 'low' and 'high' 'bits' to 'nibbles'

--- src/Tokenizer.zig
+++ src/Tokenizer.zig
@@ -7,8 +7,6 @@ const builtin = @import("builtin");
 
 const CPU = builtin.cpu;
 
-const U8_VECTOR_LEN = simd.suggestVectorLength(u8);
-
 /// The control characters of JSON.
 const CONTROL_CHARS = [_]u8{
     '[',
@@ -23,23 +21,22 @@ const CONTROL_CHARS = [_]u8{
 };
 
 /// Contains `LOW` and `HIGH` constant-arrays,
-/// indexes of which are low or high bits of `CONTROL_CHARS` elements,
-/// e.g `LOW['{' & LOW_BYTE_BITS])`, and the values of indexes are unique masks.
+/// indexes of which are low or high nibbles of `CONTROL_CHARS` elements,
+/// and the values of indexes are unique masks.
 ///
 /// Used as a lookup-table vector, from which the vector-shuffle intruction
 /// builds a new vector for searching control characters (see `next` function).
 ///
 /// Not used when the cpu target doesn't support SIMD.
-const CONTROL_CHAR_BITS = block: {
+const CONTROL_CHAR_NIBBLES = block: {
     // Fill with `0` to ensure there are falsy bits at indexes of non-control chars
     var low: [16]u8 = @splat(0);
     var high: [16]u8 = @splat(0);
 
     // Indexes are high bits of control chars
-    const highBitFlags = flagsBlock: {
+    const highNibbleFlags = flagsBlock: {
         // 8 unique flags (00000001, 00000010, etc)
         // That's enough 'cause only the ASCII chars with high bits less than 8 are used
-
         const flagsArray: [8]u8 = undefined;
 
         var flag = 1;
@@ -53,12 +50,12 @@ const CONTROL_CHAR_BITS = block: {
     };
 
     for (CONTROL_CHARS) |char| {
-        const lowCharBits = getLowBits(char);
-        const highCharBits = getHighBits(char);
+        const lowCharBits = getLowNibble(char);
+        const highCharBits = getHighNibble(char);
 
-        if (highCharBits > highBitFlags.len) @compileError("Control char is out of ASCII");
+        if (highCharBits > highNibbleFlags.len) @compileError("Control char is out of ASCII");
 
-        const flag = highBitFlags[highCharBits];
+        const flag = highNibbleFlags[highCharBits];
 
         low[lowCharBits] |= flag;
         high[highCharBits] |= flag;
@@ -70,32 +67,31 @@ const CONTROL_CHAR_BITS = block: {
     };
 };
 
+const Token = struct {};
+
 source: []const u8,
 
 pub fn init(source: []const u8) Tokenizer {
     return .{ .source = source };
 }
 
-pub fn next(self: *Tokenizer) void {}
-
-/// Returns 128, 256, 512 or `null` in case of lack of SIMD.
+/// Returns 16, 32, 64 or `null` in case of lack of SIMD.
 ///
-/// Returns 512 only if the target is `avx512bw` (which supports 64-byte vector shuffles).
-/// If the target supports `abx512` but doesn't support the mentioned shuffles, 256 is returned.
-inline fn getVectorLen_x64() ?comptime_int {
+/// Returns 64 only if the target is `avx512bw` (which supports 64-byte vector shuffles).
+/// If the target supports just `avx512`, 32 is returned.
+inline fn getVectorBytesLen_x64() ?comptime_int {
     const features = CPU.features;
     const hasFeature = Target.x86.featureSetHas;
 
     return if (hasFeature(features, .avx512bw))
-        512
+        64
     else if (hasFeature(features, .avx2))
-        256
+        32
     else if (hasFeature(features, .ssse3))
-        128
+        16
     else
         null;
 }
-
 /// Permutates elements in `vector` based on `mask` elements.
 ///
 /// Uses only 0..4 bits (low bits) of `mask` indexes,
@@ -156,7 +152,6 @@ inline fn shuffleVector512_x64(
 inline fn is128BitVector_aarch64() bool {
     return Target.aarch64.featureSetHas(CPU.features, .neon);
 }
-
 /// More preferred than `is128BitVector_aarch64` result.
 ///
 /// Returns `true` only when the `aarch64` target supports vectors with variable length (128-512 bit),
@@ -165,10 +160,15 @@ inline fn isVariableVectorLen_aarch64() bool {
     return Target.aarch64.featureSetHas(CPU.features, .sve2);
 }
 
-/// Should be called once per thread.
+/// Calling this function without
+/// checking `isVariableVectorLen_aarch64` can cause `SIGILL`.
 ///
 /// Returns the amount of bytes one vector register can occupy.
-inline fn getVariableVectorLen_aarch64() usize {
+///
+/// The result of this function should never be persisted 'cause it varies
+/// accross the CPU threads, and if the OS moves the parser
+/// to another thread during a context switch, the result can change.
+inline fn getVariableVectorBytesLen_aarch64() usize {
     return asm ("cntb %[result]"
         : [result] "=r" (-> usize),
     );
@@ -196,22 +196,21 @@ inline fn shuffleVector128_aarch64(
     );
 }
 
-/// Fills the high `byte` bits to zeros.
-fn getLowBits(byte: u8) u8 {
+/// Fills the high `byte` bits to zeros, leaving only the low nibble.
+fn getLowNibble(byte: u8) u8 {
     return byte & 0b00001111;
 }
-
 /// Moves the high `byte` bits to the low bits, filling the previous place of high bits to zeros.
-fn getHighBits(byte: u8) u8 {
+fn getHighNibble(byte: u8) u8 {
     return byte >> 4;
 }
 
 /// Fills high bits of each `vector` element to zero and leaves only the low bits.
-fn getLowBitsVector(comptime len: comptime_int, vector: @Vector(len, u8)) @Vector(len, u8) {
+fn getLowNibblesVector(comptime len: comptime_int, vector: @Vector(len, u8)) @Vector(len, u8) {
     return vector & @as(@TypeOf(vector), @splat(0b00001111));
 }
 
-/// Fills low bits to zero and moves the high bits to low bits for each `vector` element.
-fn getHighBitsVector(comptime len: comptime_int, vector: @Vector(len, u8)) @Vector(len, u8) {
+/// Fills moves the high bits to low bits of each `vector` element.
+fn getHighNibblesVector(comptime len: comptime_int, vector: @Vector(len, u8)) @Vector(len, u8) {
     return vector >> @as(@TypeOf(vector), @splat(4));
 }
