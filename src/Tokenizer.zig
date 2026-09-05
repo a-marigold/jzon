e8cf88bf0d290c4498757d41d826f1c1ee0480a1
marigold <phelixgluy@gmail.com>
2026-09-05 19:57:26 +0500

    Tokenizer: add 'getStartsOfMaskSequences' and 'getEndsOfMaskSequences' utils

--- src/Tokenizer.zig
+++ src/Tokenizer.zig
@@ -1,6 +1,7 @@
 const Tokenizer = @This();
 
 const std = @import("std");
+const math = std.math;
 const simd = std.simd;
 const Target = std.Target;
 const builtin = @import("builtin");
@@ -32,8 +33,6 @@ pub fn init(source: []const u8) Tokenizer {
 ///
 /// Used as a lookup-table vector, from which the vector-shuffle intruction
 /// builds a new vector for searching control characters (see `next` function).
-///
-/// Not used when the cpu target doesn't support SIMD.
 fn genControlCharTables() struct { lowNibbles: [16]u8, highNibbles: [8]u8 } {
     // Fill with `0` to ensure there are falsy bits at indexes of non-control chars
     var lowNibbles: [16]u8 = @splat(0);
@@ -61,7 +60,6 @@ fn genControlCharTables() struct { lowNibbles: [16]u8, highNibbles: [8]u8 } {
         if (highCharNibble > highNibbleFlags.len) @compileError("Control char is out of ASCII");
 
         const flag = highNibbleFlags[highCharNibble];
-
         lowNibbles[lowCharNibble] |= flag;
         highNibbles[highCharNibble] |= flag;
     }
@@ -70,6 +68,43 @@ fn genControlCharTables() struct { lowNibbles: [16]u8, highNibbles: [8]u8 } {
         .lowNibbles = lowNibbles,
         .highNibbles = highNibbles,
     };
+}
+
+/// For each sequence of `1` bits in an unsigned integer `mask`,
+/// leaves only the first least significant bit of the sequence.
+///
+/// Example:
+/// For `01101111` returns `00100001`
+///
+/// (Left bits: most significant, Right bits: least significant).
+inline fn getStartsOfMaskSequences(mask: anytype) @TypeOf(mask) {
+    comptime checkIsUint(@TypeOf(mask));
+
+    // Example:
+    // Mask = `0110111100000000`.
+    // Shifted = `Mask << 1` = `1101111000000000`
+    // Inverted = `~Shifted` = `0010000111111111`
+    // Result = `Mask & Inverted` = `0110111100000000` &
+    //                              `0010000111111111` =
+    //                              `0010000100000000`
+    return mask & ~(mask << 1);
+}
+
+/// For each sequence of `1` bits in an unsigned integer `mask`,
+/// leaves only the last most significant bit of the sequence, *shifted to the left by 1*.
+///
+/// *shifted to the left* means the resulting mask doesn't contain just ends of sequences,
+/// it contains ends shifted to the left by 1. To get the real ends, just do `result >> 1`.
+///
+/// Example:
+/// For `Mask = 01101111`, `Starts = 00100001`,
+/// returns `10010000` (Ends shifted by 1,
+/// to get the real ends - `10010000 >> 1`, which is `01001000`).
+inline fn getEndsOfMaskSequences(mask: anytype, startsMask: @TypeOf(mask)) @TypeOf(mask) {
+    // E.g `startsMask` is `00000100`, `mask` is `00011100`.
+    // Addition carries `startsMask` bits to the left, forming ends of sequences:
+    // `00000100` + `00111000` = `01000000`
+    return startsMask + mask;
 }
 
 /// Returns 16, 32, 64 or `null` in case of lack of SIMD.
@@ -89,12 +124,14 @@ fn getVectorLen_x64() ?comptime_int {
     else
         null;
 }
+
 /// Permutates elements in `vector` based on `mask` elements.
 ///
 /// Uses only 0..4 bits (low bits) of `mask` indexes,
 /// and if the 7 (the highest) bit equals `1`, the `result[index]` is set to `0`.
 ///
 /// Example:
+///
 /// 1. First iteration - `result[0] = vector[ mask[0] ]`.
 /// 2. Second - `result[1] = vector[ mask[1] ]`.
 /// 3. ...
@@ -223,4 +260,12 @@ fn expandComptimeVector(
     comptime newLen: comptime_int,
 ) @Vector(newLen, u8) {
     return vector ++ @as(@Vector(newLen - vector.len, u8), @splat(0));
+}
+
+/// If `T` is not an unsigned integer, shows a compile error.
+fn checkIsUint(comptime T: type) void {
+    const TInfo = @typeInfo(T);
+
+    if (TInfo != .int and TInfo.int.signedness)
+        @compileError("An integer expected");
 }
